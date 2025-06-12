8e17f1e643247fef507d3320a035c6aae36b25ad
isalnikow86 <i.salnikow@gmx.de>
2025-06-12 17:21:02 +0200

    Update train_lora.sh

--- scripts/train_lora.sh
+++ scripts/train_lora.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-accelerate launch --multi_gpu \ scripts/train_lora.py \ --config configs/lora_config.yaml
+
+accelerate launch --multi_gpu \
+  scripts/train_lora.py \
+  --config configs/lora_config.yaml
