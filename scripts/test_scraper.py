2b8bb399b748896aac272334b97eb9f9c61cac9b
isalnikow86 <i.salnikow@gmx.de>
2025-06-14 14:14:31 +0200

    Update test_scraper.py

--- scripts/test_scraper.py
+++ scripts/test_scraper.py
@@ -1,1 +1,10 @@
 
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.kindersache.de/bereiche/wissen/natur-und-mensch"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
+
+for a in soup.find_all("a", href=True):
+    print(a["href"])
