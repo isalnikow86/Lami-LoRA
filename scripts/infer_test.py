84363730ef7e11d174e1a677a5a4db2cc6a8b9b5
isalnikow86 <i.salnikow@gmx.de>
2025-06-16 17:34:39 +0200

    Update infer_test.py

--- scripts/infer_test.py
+++ scripts/infer_test.py
@@ -1,4 +1,4 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, LlamaTokenizer
 import torch
 
 MODEL_DIR = "../merged-models/combined-merged"
